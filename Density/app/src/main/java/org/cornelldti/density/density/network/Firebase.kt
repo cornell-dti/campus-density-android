8ef309fb057bdc2b2cb2434492eadd9a1b631b5e
Sam <sam@developersam.com>
2019-11-16 16:06:14 -0500

    Make Firebase compile again

--- Density/app/src/main/java/org/cornelldti/density/density/network/Firebase.kt
+++ Density/app/src/main/java/org/cornelldti/density/density/network/Firebase.kt
@@ -1,16 +1,12 @@
 package org.cornelldti.density.density.network
 
 import android.util.Log
-import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import org.cornelldti.density.density.DensityApplication
 
-class Firebase: FirebaseAuth.IdTokenListener, FirebaseAuth.AuthStateListener {
+class Firebase : FirebaseAuth.IdTokenListener, FirebaseAuth.AuthStateListener {
     var idToken: String? = null
         private set
-
-    private lateinit var auth: FirebaseAuth
 
     // Invoked whenever ID Token changed!
     override fun onIdTokenChanged(auth: FirebaseAuth) {
@@ -24,38 +20,38 @@ class Firebase: FirebaseAuth.IdTokenListener, FirebaseAuth.AuthStateListener {
 
     private fun signIn() {
         Log.d("checkpoint", "signIn")
+        val auth = FirebaseAuth.getInstance()
         auth.signInAnonymously()
-                .addOnCompleteListener(DensityApplication.getAppContext()) { task ->
-                    if (task.isSuccessful) {
-                        Log.d("checkpoint", "signIn = success")
-                        Log.d("Firebase", "signInAnonymously:success")
-                        val user = auth.currentUser
-                        requestToken(user!!)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.d("checkpoint", "signIn = failure")
-                        Log.w("Firebase", "signInAnonymously:failure", task.exception)
-                    }
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("checkpoint", "signIn = success")
+                    Log.d("Firebase", "signInAnonymously:success")
+                    val user = auth.currentUser
+                    requestToken(user!!)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.d("checkpoint", "signIn = failure")
+                    Log.w("Firebase", "signInAnonymously:failure", task.exception)
                 }
+            }
     }
 
     private fun requestToken(user: FirebaseUser) {
         Log.d("checkpoint", "requestToken")
         user.getIdToken(true)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Log.d("checkpoint", "gotToken")
-                        idToken = task.result?.token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("checkpoint", "gotToken")
+                    idToken = task.result?.token
 //                        updateUI()
-                    } else {
-                        Log.d("AUTH ERROR", "Error obtaining Firebase Auth ID token")
-                    }
+                } else {
+                    Log.d("AUTH ERROR", "Error obtaining Firebase Auth ID token")
                 }
+            }
     }
 
     // When user is signed out, or lost access.
     override fun onAuthStateChanged(auth: FirebaseAuth) {
         // TODO DISPLAY ERROR SCREEN AND ATTEMPT TO RE SIGN IN
     }
-
 }
