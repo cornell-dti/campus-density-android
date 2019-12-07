44c5d3447b074bd27e3defb3bc756f461c59de4a
Andrew <kungpaogao@gmail.com>
2019-12-07 16:10:51 -0500

    Implemented get functions for FacilityRepository

--- Density/app/src/main/java/org/cornelldti/density/density/FacilityRepository.kt
+++ Density/app/src/main/java/org/cornelldti/density/density/FacilityRepository.kt
@@ -6,11 +6,41 @@ import org.cornelldti.density.density.data.FacilityClass
 import org.cornelldti.density.density.network.API
 
 class FacilityRepository {
-    fun getFacilities(): LiveData<FacilityClass> {
-        val data = MutableLiveData<MutableList<FacilityClass>>()
-//        api.getFacilities().
 
+    private val api: API = API(context = DensityApplication.getAppContext())
 
-        TODO()
+    fun getFacilities(): LiveData<List<FacilityClass>> {
+        val facilities = MutableLiveData<List<FacilityClass>>()
+        api.fetchFacilities(
+                onDone = { response -> facilities.value = response },
+                onError = { _ -> TODO() },
+                success = {}
+        )
+
+        return facilities
+    }
+
+    fun getHistoricalData(day: String, facilityId: String): LiveData<List<Double>> {
+        val densities = MutableLiveData<List<Double>>()
+        api.fetchHistoricalJSON(
+                day = day,
+                facilityId = facilityId,
+                fetchOperatingHoursOnResponse = {},
+                fetchHistoricalJSONOnResponse = { response -> densities.value = response }
+        )
+
+        return densities
+    }
+
+    fun getOperatingHours(day: String, facilityId: String): LiveData<List<String>> {
+        val hours = MutableLiveData<List<String>>()
+        api.fetchHistoricalJSON(
+                day = day,
+                facilityId = facilityId,
+                fetchOperatingHoursOnResponse = { response -> hours.value = response },
+                fetchHistoricalJSONOnResponse = {}
+        )
+
+        return hours
     }
 }
