51774d33234710627aa597062e3fce8cf82d7c13
kaushikravikumar <kaushik.ravikumar1999@gmail.com>
2020-10-03 23:00:15 -0400

    changed operatinghoursclass to data class

--- Density/app/src/main/java/org/cornelldti/density/density/data/OperatingHoursClass.kt
+++ Density/app/src/main/java/org/cornelldti/density/density/data/OperatingHoursClass.kt
@@ -1,10 +1,3 @@
 package org.cornelldti.density.density.data
 
-class OperatingHoursClass(todayOperatingHours: List<Pair<Long, Long>>, tomorrowFirstOpHours: Pair<Long, Long>) {
-    lateinit var todayOperatingHours: List<Pair<Long, Long>>
-    lateinit var tomorrowFirstOpHours: Pair<Long, Long>
-    init {
-        this.todayOperatingHours = todayOperatingHours
-        this.tomorrowFirstOpHours = tomorrowFirstOpHours
-    }
-}
+data class OperatingHoursClass(val todayOperatingHours: List<Pair<Long, Long>>, val tomorrowFirstOpHours: Pair<Long, Long>)
